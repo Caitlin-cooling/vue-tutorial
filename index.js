31ef4e466aed6a1137dd1da273c8d6a508519a3a
Caitlin Cooling <ccooling97@gmail.com>
2019-02-18 15:39:54 +0000

    Using v-for to iterate over an array of todos

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello, world!',
-    seen: true
+    seen: true,
+    todos: [
+      { text: 'One thing'},
+      { text: 'Another thing' },
+      { text: 'And another' }
+    ]
   }
 });
